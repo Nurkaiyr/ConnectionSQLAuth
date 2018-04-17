d9ff3d08534d1add9285dd2c743344651ca6c99a
Едиге Нуркайыр Маратулы <@corp.itstep>
2018-04-17 10:26:32 +0600

    done

---
+++ ConnectingToMyDB/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectingToMyDB
+{
+    public class Connection
+    {
+        public SqlConnection GetConnection()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.UserID = "Vasya";
+            builder.Password = "bulka1";
+            builder.DataSource = "(local)";
+            builder.InitialCatalog = "MyDB";
+            builder.IntegratedSecurity = false; 
+
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            return connection;
+        }
+    }
+}
